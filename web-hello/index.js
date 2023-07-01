85a967c6ed12edac5d9274b677a9ca039cc61f8a
haraphat01 <haraphat@gmail.com>
2023-07-01 05:27:05 -0700

    rolling the new version

--- web-hello/index.js
+++ web-hello/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!, Arafat is learning kubernetes');
+  res.send('Hello World!, Arafat is learning kubernetes and docker as version 2 rolls out');
 });
 
 app.listen(3000, () => {
